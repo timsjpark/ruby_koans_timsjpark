ac283a191c179ba6bc0b96e459c26db5463224be
Tim Park <timsjpark@gmail.com>
2015-10-25 19:49:49 -0600

    Modify about_dice_project.rb to pass koan
    [#104807422]

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -1,10 +1,19 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+
+class DiceSet
+  def roll(number)
+    @values = []
+    number.times do
+      @values.push(rand(1..6))
+    end
+  end
+
+  def values
+    @values
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set

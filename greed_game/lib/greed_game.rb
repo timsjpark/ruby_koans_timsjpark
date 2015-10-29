d1090d97c691eb7a274ddf7bc4cd6cd27883203f
Tim Park <timsjpark@gmail.com>
2015-10-28 18:08:47 -0600

    Remove DiceSet class and associated methods from greed_game.rb
    [#104807438]

--- greed_game/lib/greed_game.rb
+++ greed_game/lib/greed_game.rb
@@ -1,3 +1,5 @@
+require './dice'
+
 # Greed game class
 class GreedGame
   attr_reader :players, :scoresheet
@@ -22,59 +24,4 @@ class GreedGame
       'You must start the game with at least 2 players!'
     end
   end
-end
-
-# DiceSet class
-class DiceSet
-  attr_reader :values
-
-  def roll(number)
-    @values = []
-    number.times do
-      @values.push(rand(1..6))
-    end
-  end
-end
-
-# From about_scoring_project.rb
-
-def score(dice)
-  rolls = {1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0}
-  dice.each { |single_roll| rolls[single_roll] += 1 }
-  compute_score(rolls)
-end
-
-def compute_score(rolls)
-  score = 0
-  score = ones(rolls, score)
-  score = fives(rolls, score)
-  score = other_triples(rolls, score)
-  score
-end
-
-def ones(rolls, score)
-  if rolls[1] >= 3
-    score += 1000
-    score += 100 * (rolls[1] - 3)
-  else
-    score += 100 * rolls[1]
-  end
-  score
-end
-
-def fives(rolls, score)
-  if rolls[5] >= 3
-    score += 500
-    score += 50 * (rolls[5] - 3)
-  else
-    score += 50 * rolls[5]
-  end
-  score
-end
-
-def other_triples(rolls, score)
-  [2, 3, 4, 6].each do |key|
-    score += (100 * key) if rolls[key] >= 3
-  end
-  score
 end
